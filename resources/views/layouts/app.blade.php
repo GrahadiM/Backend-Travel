a9943c9332967d30c6b812bff52d83aa7224067c
abdurrahmangrahadim <hadi@busanid.dev>
2024-06-23 16:39:42 +0700

    add file .env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{!! config('app.name', 'Let&#39;s Go Bandung') !!}</title>
 
     <!-- Fonts & Icons -->
     <link
